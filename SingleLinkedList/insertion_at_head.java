5d802622d6c9d833836bbd9b577e346e21c79867
maniharan <148434634+katukojwalamaniharan@users.noreply.github.com>
2025-04-19 12:14:31 +0530

    Update insertion_at_head.java

--- SingleLinkedList/insertion_at_head.java
+++ SingleLinkedList/insertion_at_head.java
@@ -1,1 +1,30 @@
+Definition for singly Linked List
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+
+class Solution {
+    public ListNode insertAtHead(ListNode head, int X) {
+        ListNode newnode =new ListNode(X);
+        newnode.next=head;
+        head=newnode;
+        return head;
+    }
+}
 
